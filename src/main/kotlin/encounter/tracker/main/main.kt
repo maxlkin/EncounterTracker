1781e1543a03b341a0301d9de03a447f3ceba1ea
Max Larkin <maxlarkin@protonmail.com>
2020-11-02 15:56:39 +0000

    Adding create/read/update/delete queries for characters

--- src/main/kotlin/encounter/tracker/main/main.kt
+++ src/main/kotlin/encounter/tracker/main/main.kt
@@ -3,6 +3,7 @@ import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import encounter.tracker.db.Database
 import encounter.tracker.views.Root
+import encountertrackerdb.MainQueries
 import tornadofx.App
 import tornadofx.launch
 
@@ -10,9 +11,17 @@ class EncounterTracker: App(Root::class)
 
 fun main(args: Array<String>) {
     // Getting database connection.
+
     val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-    // Creating Schema
+
     Database.Schema.create(driver)
+    val database = Database(driver)
+    val mainQueries: MainQueries = database.mainQueries
+    mainQueries.insertCharacter("Johnny Two Fingers", 12, 1, 20, 20)
+    mainQueries.updateCharacterByID("Johnny Three Fingers", 12, 1, 20, 20, 3)
+    println(mainQueries.selectCharacterByID(1).executeAsList())
+    println(mainQueries.selectAllCharacters().executeAsList())
+    // Creating Schema
     // Launching App (TornadoFX)
     launch<EncounterTracker>(args)
 }
