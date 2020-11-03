54de8fe179b398b2c0568d81754ea4b0fddaa995
Max Larkin <maxlarkin@protonmail.com>
2020-11-03 13:26:27 +0000

    Characters can be selected

--- src/main/kotlin/encounter/tracker/views/CharacterView.kt
+++ src/main/kotlin/encounter/tracker/views/CharacterView.kt
@@ -3,12 +3,23 @@ import encounter.tracker.controllers.CharacterController
 import encountertrackerdb.Character
 import javafx.geometry.Pos
 import javafx.scene.control.SelectionMode
+import javafx.scene.control.TextField
 import tornadofx.*
 import tornadofx.WizardStyles.Companion.graphic
 
 class CharacterView: View() {
     private val controller: CharacterController by inject()
+
+    var idField : TextField by singleAssign()
+    var nameField : TextField by singleAssign()
+    var armorClassField : TextField by singleAssign()
+    var initiativeField : TextField by singleAssign()
+    var maxHealthField : TextField by singleAssign()
+    var currentHealthField : TextField by singleAssign()
+
     override val root = vbox(10) {
+
+
         addClass(Style.view)
         label("Characters") {
             style {
@@ -19,14 +30,14 @@ class CharacterView: View() {
             fieldset("Character Details") {
                 vbox {
                     hbox(20) {
-                        field("ID") { textfield() }
-                        field("Name") { textfield() }
-                        field("Armor Class") { textfield() }
+                        field("ID") { textfield() { idField = this } }
+                        field("Name") { textfield() { nameField = this } }
+                        field("Armor Class") { textfield() { armorClassField = this } }
                     }
                     hbox(20) {
-                        field("Initiative") { textfield() }
-                        field("Max Health") { textfield() }
-                        field("Current Health") { textfield() }
+                        field("Initiative") { textfield() { initiativeField = this } }
+                        field("Max Health") { textfield() { maxHealthField = this } }
+                        field("Current Health") { textfield() { currentHealthField = this } }
                     }
                 }
             }
@@ -36,11 +47,13 @@ class CharacterView: View() {
             button("Filter")
             button("Add Character")
             button("Update Character")
+            button("Clear Selection")
         }
 
         val tableData = controller.getCharacterList()
 
         tableview(tableData) {
+            selectionModel.selectionMode = SelectionMode.SINGLE
             readonlyColumn("ID", Character::id)
             readonlyColumn("Name", Character::name)
             readonlyColumn("Armor Class", Character::armor_class)
@@ -52,11 +65,21 @@ class CharacterView: View() {
                     button("Delete") {
                         action {
                             controller.deleteCharacter(it)
-                            println(controller.getCharacterList().asObservable())
                             tableData.setAll(controller.getCharacterList())
                         }
                     }
                 }
+            }
+            onLeftClick {
+                val character = this.selectedItem!!
+                idField.text = character.id.toString()
+                idField.setEditable(false)
+                idField.setDisable(true)
+                nameField.text = character.name
+                armorClassField.text = character.armor_class.toString()
+                initiativeField.text = character.initiative_modifier.toString()
+                maxHealthField.text = character.max_health.toString()
+                currentHealthField.text = character.current_health.toString()
             }
         }
     }
