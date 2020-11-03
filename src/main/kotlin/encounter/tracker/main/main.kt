358d9170da0b0604087ab05d3a4166fa8e4d1268
Max Larkin <maxlarkin@protonmail.com>
2020-11-03 21:36:21 +0000

    Adding logging dependancy and database setup checking

--- src/main/kotlin/encounter/tracker/main/main.kt
+++ src/main/kotlin/encounter/tracker/main/main.kt
@@ -4,10 +4,18 @@ import encounter.tracker.views.Root
 import encounter.tracker.views.Style
 import tornadofx.App
 import tornadofx.launch
-
+import mu.KotlinLogging
 class EncounterTracker: App(Root::class, Style::class)
+private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
+    // Checking database setup
+    //val driver = Database.getConnection()
+    try {
+        Database.setup()
+    } catch (e: Exception) {
+        logger.info("Skipping database setup. May already exist.")
+    }
     // Launching App (TornadoFX)
     launch<EncounterTracker>(args)
 }
